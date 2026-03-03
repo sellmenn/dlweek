5ecc644616f163c79d23dc7ba6e630cc4fab7e80
yjjunnie <161452557+yjjunnie@users.noreply.github.com>
2026-03-03 11:17:45 +0800

    Update cluster.ts

--- resnet/src/types/cluster.ts
+++ resnet/src/types/cluster.ts
@@ -1,6 +1,17 @@
 export interface Cluster {
+<<<<<<< Updated upstream
     name: string;
     centroid: [number, number];
     count: number;
     combined_severity: 'little_or_none' | 'mild' | 'severe';
+=======
+    name: string,
+    centroid: {
+        lat: number,
+        long: number
+    },
+    count: number,
+    severity_score: number,
+    severity_class: "low" | "medium" | "high"
+>>>>>>> Stashed changes
 }
