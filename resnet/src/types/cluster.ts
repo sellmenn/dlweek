af78b33e29a3f99f95db63fb5a6e9b0ebf12b980
yjjunnie <161452557+yjjunnie@users.noreply.github.com>
2026-03-03 11:17:19 +0800

    some changes to use the correct endpts

--- resnet/src/types/cluster.ts
+++ resnet/src/types/cluster.ts
@@ -1,20 +1,6 @@
 export interface Cluster {
-<<<<<<< Updated upstream
     name: string;
     centroid: [number, number];
     count: number;
     combined_severity: 'little_or_none' | 'mild' | 'severe';
-=======
-    name: string,
-    centroid: {
-        lat: number,
-        long: number
-    },
-    count: number,
-    severity_score: number,
-    severity_class: "low" | "medium" | "high"
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
